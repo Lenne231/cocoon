8868baf6048b3454c2eb13f74a7541449ea3aa94
Lennart Schlegge <lennart_schlegge@msn.com>
2024-02-12 23:14:55 +0100

    ...

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -7,8 +7,8 @@ import { About } from './sections/About';
 export function App() {
   return <div className='d-flex'>
     <Intro />
-    <Events />
     <About />
+    <Events />
     <Contact />
   </div>;
 }
--- packages/app/src/sections/Intro.tsx
+++ packages/app/src/sections/Intro.tsx
@@ -4,5 +4,6 @@ export function Intro() {
   return <div>
     <img src={logoUri} alt="cocoon" />
     <div className="text-2xl text-center">Cafe - Bar - Events</div>
+    <div>About us / Impressions / get to know us (should be a gallery with markdown text, like a storybook)</div>
   </div>
 }
