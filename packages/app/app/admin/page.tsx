37c152b3347278cde67267462089e9e21a408e44
Lennart Schlegge <lennart_schlegge@msn.com>
2024-02-21 23:26:27 +0100

    ...

--- packages/app/app/admin/page.tsx
+++ packages/app/app/admin/page.tsx
@@ -1,6 +1,7 @@
 "use client";
+
 if(typeof window !== 'undefined') {
-  import('./init').then(i => i.init());
+  import('./init').then(i => i.init()).catch(console.error);
 }
 
 export default function Admin() {
